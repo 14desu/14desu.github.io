6dea49f95a84912f7bdfb927c20f6d4106159f5b
14desu <85554169+14desu@users.noreply.github.com>
2023-03-08 13:50:06 +0900

    Add files via upload

---
+++ common/navbar_load.js
@@ -0,0 +1,6 @@
+$(document).ready( function() {
+    // navbar load
+    $("#navbar").load("https://14desu.github.io/common/navbar.html");
+    // sidebar load
+    $("#sidenavAccordion").load("https://14desu.github.io/common/sidebar.html");
+});
