639ee8fb4e57c676140f57c183b71ebc73ce6efa
14desu <85554169+14desu@users.noreply.github.com>
2023-03-12 11:58:18 +0900

    Add files via upload

--- NFLab/aircrft_info.js
+++ NFLab/aircrft_info.js
@@ -3,39 +3,39 @@ var kr_torpedoinfo_api = [];
 var kr_bomberinfo_api = [];
 var kr_scoutinfo_api = [];
 
-$(document).ready( function() {
+$(document).ready(function () {
     kr_aircraft_info_api();
 });
 
-$(document).on("change",".fighter_input",function(){
+$(document).on("change", ".fighter_input", function () {
     let table = $('#fighter_output').DataTable();
     table.destroy();
     fighter_output();
 });
 
-$(document).on("change",".torpedo_input",function(){
+$(document).on("change", ".torpedo_input", function () {
     let table = $('#torpedo_output').DataTable();
     table.destroy();
     torpedo_output();
 });
 
-$(document).on("change",".bomber_input",function(){
+$(document).on("change", ".bomber_input", function () {
     let table = $('#bomber_output').DataTable();
     table.destroy();
     bomber_output();
 });
 
-$(document).on("change",".scout_input",function(){
+$(document).on("change", ".scout_input", function () {
     let table = $('#scout_output').DataTable();
     table.destroy();
     scout_output();
 });
 
-function kr_aircraft_info_api(){
+function kr_aircraft_info_api() {
 
     const kr_aircraft_info_api_URL = "https://script.google.com/macros/s/AKfycbwky1zdTmBQVPUGk5Nby-I99NWnzHalxIY5CtLU_sMNfZEaXQHrl1mkMaCeIxOHAVyE/exec";
 
-    $.getJSON(kr_aircraft_info_api_URL, function(response) {
+    $.getJSON(kr_aircraft_info_api_URL, function (response) {
         kr_fighterinfo_api = response["data"]["fighter"];
         fighter_output();
         kr_torpedoinfo_api = response["data"]["torpedo"];
@@ -44,25 +44,25 @@ function kr_aircraft_info_api(){
         bomber_output();
         kr_scoutinfo_api = response["data"]["scout"];
         scout_output();
-//    $("#test").html(kr_torpedoinfo_api);
-});
-    
+        //    $("#test").html(kr_torpedoinfo_api);
+    });
+
 }
 
 
-function fighter_output(){
+function fighter_output() {
     let kr_fighterinfo_filter = [];
     let fighter_filter = [];
 
-    for(i=0; i<5; i++){
-        if ($('#fighter_input'+[i+1]).is(':checked')){
-            fighter_filter[i] = $('#fighter_input'+[i+1]).val();
+    for (i = 0; i < 5; i++) {
+        if ($('#fighter_input' + [i + 1]).is(':checked')) {
+            fighter_filter[i] = $('#fighter_input' + [i + 1]).val();
         }
     }
 
-    for(i=0; i<kr_fighterinfo_api.length; i++){
-        for(j=0; j<5; j++){
-            if(kr_fighterinfo_api[i][15] == fighter_filter[j]){
+    for (i = 0; i < kr_fighterinfo_api.length; i++) {
+        for (j = 0; j < 5; j++) {
+            if (kr_fighterinfo_api[i][15] == fighter_filter[j]) {
                 kr_fighterinfo_filter.push(kr_fighterinfo_api[i]);
             }
         }
@@ -70,7 +70,7 @@ function fighter_output(){
 
     $('#fighter_output').DataTable({
         data: kr_fighterinfo_filter,
-        order: [],
+        order: [[4, "desc"]],
         retrieve: true,
         paging: false,
         searching: false,
@@ -101,19 +101,19 @@ function fighter_output(){
     });
 }
 
-function torpedo_output(){
+function torpedo_output() {
     let kr_torpedoinfo_filter = [];
     let torpedo_filter = [];
 
-    for(i=0; i<3; i++){
-        if ($('#torpedo_input'+[i+1]).is(':checked')){
-            torpedo_filter[i] = $('#torpedo_input'+[i+1]).val();
+    for (i = 0; i < 3; i++) {
+        if ($('#torpedo_input' + [i + 1]).is(':checked')) {
+            torpedo_filter[i] = $('#torpedo_input' + [i + 1]).val();
         }
     }
 
-    for(i=0; i<kr_torpedoinfo_api.length; i++){
-        for(j=0; j<3; j++){
-            if(kr_torpedoinfo_api[i][15] == torpedo_filter[j]){
+    for (i = 0; i < kr_torpedoinfo_api.length; i++) {
+        for (j = 0; j < 3; j++) {
+            if (kr_torpedoinfo_api[i][15] == torpedo_filter[j]) {
                 kr_torpedoinfo_filter.push(kr_torpedoinfo_api[i]);
             }
         }
@@ -121,7 +121,7 @@ function torpedo_output(){
 
     $('#torpedo_output').DataTable({
         data: kr_torpedoinfo_filter,
-        order: [],
+        order: [[4, "desc"]],
         retrieve: true,
         paging: false,
         searching: false,
@@ -154,19 +154,19 @@ function torpedo_output(){
     });
 }
 
-function bomber_output(){
+function bomber_output() {
     let kr_bomberinfo_filter = [];
     let bomber_filter = [];
 
-    for(i=0; i<4; i++){
-        if ($('#bomber_input'+[i+1]).is(':checked')){
-            bomber_filter[i] = $('#bomber_input'+[i+1]).val();
+    for (i = 0; i < 4; i++) {
+        if ($('#bomber_input' + [i + 1]).is(':checked')) {
+            bomber_filter[i] = $('#bomber_input' + [i + 1]).val();
         }
     }
 
-    for(i=0; i<kr_bomberinfo_api.length; i++){
-        for(j=0; j<4; j++){
-            if(kr_bomberinfo_api[i][15] == bomber_filter[j]){
+    for (i = 0; i < kr_bomberinfo_api.length; i++) {
+        for (j = 0; j < 4; j++) {
+            if (kr_bomberinfo_api[i][15] == bomber_filter[j]) {
                 kr_bomberinfo_filter.push(kr_bomberinfo_api[i]);
             }
         }
@@ -174,7 +174,7 @@ function bomber_output(){
 
     $('#bomber_output').DataTable({
         data: kr_bomberinfo_filter,
-        order: [],
+        order: [[4, "desc"]],
         retrieve: true,
         paging: false,
         searching: false,
@@ -206,19 +206,19 @@ function bomber_output(){
     });
 }
 
-function scout_output(){
+function scout_output() {
     let kr_scoutinfo_filter = [];
     let scout_filter = [];
 
-    for(i=0; i<5; i++){
-        if ($('#scout_input'+[i+1]).is(':checked')){
-            scout_filter[i] = $('#scout_input'+[i+1]).val();
+    for (i = 0; i < 5; i++) {
+        if ($('#scout_input' + [i + 1]).is(':checked')) {
+            scout_filter[i] = $('#scout_input' + [i + 1]).val();
         }
     }
 
-    for(i=0; i<kr_scoutinfo_api.length; i++){
-        for(j=0; j<5; j++){
-            if(kr_scoutinfo_api[i][15] == scout_filter[j]){
+    for (i = 0; i < kr_scoutinfo_api.length; i++) {
+        for (j = 0; j < 5; j++) {
+            if (kr_scoutinfo_api[i][15] == scout_filter[j]) {
                 kr_scoutinfo_filter.push(kr_scoutinfo_api[i]);
             }
         }
@@ -226,7 +226,7 @@ function scout_output(){
 
     $('#scout_output').DataTable({
         data: kr_scoutinfo_filter,
-        order: [],
+        order: [[4, "desc"]],
         retrieve: true,
         paging: false,
         searching: false,
